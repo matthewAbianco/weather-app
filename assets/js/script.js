aac3ebc17ebb405b99393f4ecb3369f10b6d040e
Matt Bianco <matthewbianco@protonmail.com>
2021-01-22 13:36:28 -0500

    added dom tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,49 @@
+// API key
+var key = "813dabee4e4e5ea4aecea876113e9598";
+
+// search bar
+var searchedCity = $("#search-box");
+var searchButton = $("search-button");
+
+// weather info
+var cityTitle = $("current-city-name");
+var weatherIcon = $("weather-icon");
+var temperature = $(".temperature");
+var humidity = $(".humidity");
+var windSpeed = $(".wind-speed");
+var uvIndex = $("uv-index");
 
 
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=4c67d1c852a0404242197e390eff43e5", function(weatherData){
-        console.log(weatherData);
 
-    //var weatherIcon = 
-})
+var cityName = searchedCity.value;
+    if (cityName) {
+        getCityName(cityName);
+
+        // clear search value
+        searchedCity.textContent = "";
+
+    } else {
+        alert("Pick a city");
+    };
+
+    searchButton.addEventListener("click", function(event){
+        event.preventDefault();
+        let textCityName = searchedCity.val();
+        localStorage.setItem("Name", textCityName);
+        console.log(textCityName);
+    });
+
+    searchedCity.val(localStorage.getItem("Name"))
+
+    $('#date');
+
+function update() {
+  $('#date').html(moment().format("MMM Do YY"));
+
+
+setInterval(update, 600,000);
+
+}
+update();
+
+searchButton.addEventListener("click", searchButtonEvent);
