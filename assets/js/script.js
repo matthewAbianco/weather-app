b8b0c2809a78646cd1186b5616deefe286ea9d01
Matt Bianco <matthewbianco@protonmail.com>
2021-01-22 16:09:21 -0500

    added date elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,41 @@
 var key = "813dabee4e4e5ea4aecea876113e9598";
 
 // search bar
-var searchedCity = $("#search-box");
-var searchButton = $("search-button");
-
-// weather info
-var cityTitle = $("current-city-name");
-var weatherIcon = $("weather-icon");
-var temperature = $(".temperature");
-var humidity = $(".humidity");
-var windSpeed = $(".wind-speed");
-var uvIndex = $("uv-index");
+var cityUv = $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=4c67d1c852a0404242197e390eff43e5")
+    console.log(cityUv);
+var lat = cityUv.responseJSON;
+console.log(lat);
+var lon = Math.floor(cityUv.responseJSON);
+console.log(lon);
 
 
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=4c67d1c852a0404242197e390eff43e5", function(currentWeatherData){
+        console.log(currentWeatherData);
 
-var cityName = searchedCity.value;
-    if (cityName) {
-        getCityName(cityName);
+        var icon = "https://openweathermap.org/img/w/" + currentWeatherData.weather[0].icon + ".png";
+        console.log(icon);
 
-        // clear search value
-        searchedCity.textContent = "";
+        var temp = Math.floor(currentWeatherData.main.temp);
+        var cityName = currentWeatherData.name;
+        var humidity = currentWeatherData.main.humidity;
 
-    } else {
-        alert("Pick a city");
-    };
+        $("#city-name").append(cityName)
+        $('#date').append();
+        $(".weather-icon").attr("src", icon);
+       
+        $(".temperature").append(temp);
+        $(".humidity").append(humidity);
+
+        
+}); 
+
+
+
+  var violet = $.getJSON("http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=4c67d1c852a0404242197e390eff43e5", function(uv){
+   console.log(uv);
+
+        $("#uv-index").append(uv);
+});
 
     searchButton.addEventListener("click", function(event){
         event.preventDefault();
@@ -35,15 +47,13 @@ var cityName = searchedCity.value;
 
     searchedCity.val(localStorage.getItem("Name"))
 
-    $('#date');
+$('#date');
 
 function update() {
   $('#date').html(moment().format("MMM Do YY"));
 
-
 setInterval(update, 600,000);
-
-}
+};
 update();
 
 searchButton.addEventListener("click", searchButtonEvent);
