036593565163217257ea90cc42d8df9eac7efceb
Matt Bianco <matthewbianco@protonmail.com>
2021-01-21 22:47:23 -0500

    got the weather data fetched locally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-var API = "4c67d1c852a0404242197e390eff43e5";
+
+
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=4c67d1c852a0404242197e390eff43e5", function(weatherData){
+        console.log(weatherData);
+
+    //var weatherIcon = 
+})
